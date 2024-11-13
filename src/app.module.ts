e044ad5e6cecb9229de6b1ea304b63ca6cbed506
MiriamPhiri <bsc-com-13-22@unima.ac.mw>
2024-11-13 09:34:18 +0200

    added user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
