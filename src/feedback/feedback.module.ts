d3a98fe1b27b1d409c44fd3f6f4d1a67730d276c
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-11-30 21:36:31 +0200

    imported the user module

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -3,9 +3,10 @@ import { FeedbackController } from './feedback.controller';
 import { FeedbackService } from './feedback.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Feedback } from './feedback.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Feedback])],
+  imports: [UserModule,TypeOrmModule.forFeature([Feedback])],
   controllers: [FeedbackController],
   providers: [FeedbackService]
 })
