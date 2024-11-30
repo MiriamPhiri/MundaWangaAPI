7ad0e145ba7a9d8085c1da347c0a861b548922a3
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-11-30 22:31:52 +0200

    created the account-request entity

---
+++ src/account-request/account-request.entity.ts
@@ -0,0 +1,15 @@
+import { Feedback } from "src/feedback/feedback.entity";
+import { User } from "src/user/user.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class AccountRequest{
+@PrimaryGeneratedColumn()
+id: number;
+
+@Column()
+request: 'Account-Deletion' | 'Password-Reset';
+
+@ManyToOne(()=>User , (user: User )=> {user.account_requests} )
+user: User;
+}
